f5b994081c1d418a6aa737b427088c9955f56c41
jssuzanne <jssuzanne@anybox.fr>
2017-07-23 01:56:28 +0200

    Update license

--- anyblok_furetui/blok_manager/__init__.py
+++ anyblok_furetui/blok_manager/__init__.py
@@ -1,3 +1,11 @@
+# -*- coding: utf-8 -*-
+# This file is a part of the AnyBlok project
+#
+#    Copyright (C) 2017 Jean-Sebastien SUZANNE <jssuzanne@anybox.fr>
+#
+# This Source Code Form is subject to the terms of the Mozilla Public License,
+# v. 2.0. If a copy of the MPL was not distributed with this file,You can
+# obtain one at http://mozilla.org/MPL/2.0/.
 from anyblok.blok import Blok
 from anyblok_furetui.release import version
 from logging import getLogger
