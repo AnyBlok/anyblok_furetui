01684c782fcbd41740c187a19d85288072c427a9
jssuzanne <jssuzanne@anybox.fr>
2017-07-23 01:26:42 +0200

    Update documentation

--- doc/conf.py
+++ doc/conf.py
@@ -23,6 +23,7 @@
 
 
 # -- General configuration ------------------------------------------------
+import sphinx_bootstrap_theme
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
@@ -31,7 +32,9 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -83,7 +86,8 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'bootstrap'
+html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
