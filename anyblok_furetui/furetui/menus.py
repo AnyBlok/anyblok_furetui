b20474f8fa78bcd352b251a1963accc45806fa82
Hugo QUEZADA <h.quezada@sensee.com>
2019-11-14 16:57:14 +0100

    Furet with MsSQL: A column cannot be specified more than once in order by

--- anyblok_furetui/furetui/menus.py
+++ anyblok_furetui/furetui/menus.py
@@ -3,6 +3,7 @@
 #
 #    Copyright (C) 2017 Jean-Sebastien SUZANNE <jssuzanne@anybox.fr>
 #    Copyright (C) 2019 Jean-Sebastien SUZANNE <js.suzanne@gmail.com>
+#    Copyright (C) 2019 Hugo QUEZADA <gohu@hq.netlib.re>
 #
 # This Source Code Form is subject to the terms of the Mozilla Public License,
 # v. 2.0. If a copy of the MPL was not distributed with this file,You can
@@ -124,7 +125,6 @@ class Sub:
         # TODO raise if MENU_TYPE is None
         query = cls.query()
         query = query.filter(cls.parent == parent)
-        query = query.order_by(cls.sequence)
         query = cls.update_query_from_authenticated_id(
             query, authenticated_userid)
 
