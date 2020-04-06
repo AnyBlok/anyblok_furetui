4c4eadde608bb56c0a68e6c744cc185c32c58422
Pierre Verkest <pverkest@anybox.fr>
2020-04-06 15:06:32 +0200

    line was too long

--- anyblok_furetui/furetui/resource.py
+++ anyblok_furetui/furetui/resource.py
@@ -99,7 +99,9 @@ class Template:
     def get_field_for_File(self, field, description, fields2read):
         return self.get_field_for_(field, 'File', description, fields2read)
 
-    def get_field_for_Many2One(self, field, description, fields2read):  # noqa: C901
+    def get_field_for_Many2One(
+        self, field, description, fields2read
+    ):  # noqa: C901
         Model = self.registry.get(description['model'])
         description = description.copy()
         display = field.attrib.get('display')
