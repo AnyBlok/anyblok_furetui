a7b7f50f8657d3c794b22e12f169ae3e5ba42286
jssuzanne <js.suzanne@gmail.com>
2020-04-09 09:54:05 +0200

    Flake8

--- anyblok_furetui/conftest.py
+++ anyblok_furetui/conftest.py
@@ -5,6 +5,6 @@
 # This Source Code Form is subject to the terms of the Mozilla Public License,
 # v. 2.0. If a copy of the MPL was not distributed with this file,You can
 # obtain one at http://mozilla.org/MPL/2.0/.
-import pytest
+import pytest  # noqa
 from anyblok.tests.conftest import *  # noqa
 from anyblok_pyramid.conftest import *  # noqa
