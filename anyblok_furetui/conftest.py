0ad5831bacc2035e95d0292b25d9d966a7e526ed
jssuzanne <js.suzanne@gmail.com>
2020-04-09 09:49:01 +0200

    Updated conftest

--- anyblok_furetui/conftest.py
+++ anyblok_furetui/conftest.py
@@ -1,80 +1,10 @@
 # This file is a part of the AnyBlok project
 #
-#    Copyright (C) 2018 Denis VIVIÈS <dvivies@geoblink.com>
+#    Copyright (C) 2020 Jean-Sebastien SUZANNE <js.suzanne@gmail.com>
+#
 # This Source Code Form is subject to the terms of the Mozilla Public License,
 # v. 2.0. If a copy of the MPL was not distributed with this file,You can
 # obtain one at http://mozilla.org/MPL/2.0/.
-
-from logging import getLogger
-
 import pytest
-import sqlalchemy
-from sqlalchemy import event
-
-from anyblok.config import Configuration
-from anyblok.registry import RegistryManager
-from anyblok.blok import BlokManager
-
-logger = getLogger(__name__)
-
-
-@pytest.fixture(scope='session')
-def init_session(request):
-    # Init registry
-    additional_setting = {'unittest': True}
-
-    if len(BlokManager.list()) == 0:
-        BlokManager.load()
-    registry = RegistryManager.get(Configuration.get('db_name'),
-                                   **additional_setting)
-    registry.commit()
-    # Add savepoint
-    registry.begin_nested()
-
-    @event.listens_for(registry.session, 'after_transaction_end')
-    def restart_savepoint(session, transaction):
-        if transaction.nested and not transaction._parent.nested:
-            session.expire_all()
-            session.begin_nested()
-
-    logger.info('Creating registry')
-    yield registry
-
-    request.addfinalizer(registry.session.close)
-
-
-@pytest.fixture(scope='function')
-def rollback_registry(request, init_session):
-    registry = init_session
-
-    yield registry
-
-    def clean_up():
-        try:
-            logger.debug('Invalidating all cache')
-            registry.System.Cache.invalidate_all()
-        except sqlalchemy.exc.InvalidRequestError:
-            logger.warning('Invalid request Error: while invalidating all '
-                           'caches after {}'
-                           .format(request.function.__name__))
-        finally:
-            registry.rollback()
-
-    request.addfinalizer(clean_up)
-
-
-# Pyramid
-from webtest import TestApp  # noqa
-from anyblok_pyramid.pyramid_config import Configurator  # noqa
-
-
-@pytest.fixture(scope="session")
-def webserver(request, init_session):
-    config = Configurator()
-    config.include_from_entry_point()
-    # No param here # for includeme in self.includemes:
-    # No param here #     config.include(includeme)
-
-    config.load_config_bloks()
-    app = config.make_wsgi_app()
-    return TestApp(app)
+from anyblok.tests.conftest import *  # noqa
+from anyblok_pyramid.conftest import *  # noqa
