446dbe06eab492625f6b577f34bc3bebf386372c
jssuzanne <js.suzanne@gmail.com>
2020-04-09 09:51:30 +0200

    Updated conftest

--- anyblok_furetui/furetui/tests/conftest.py
+++ anyblok_furetui/furetui/tests/conftest.py
@@ -1,8 +1,8 @@
-# -*- coding: utf-8 -*-
 # This file is a part of the AnyBlok project
 #
-#    Copyright (C) 2017 Jean-Sebastien SUZANNE <jssuzanne@anybox.fr>
+#    Copyright (C) 2019 Jean-Sebastien SUZANNE <js.suzanne@gmail.com>
 #
 # This Source Code Form is subject to the terms of the Mozilla Public License,
 # v. 2.0. If a copy of the MPL was not distributed with this file,You can
 # obtain one at http://mozilla.org/MPL/2.0/.
+from anyblok.tests.conftest import *  # noqa
