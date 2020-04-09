c1d479211d10a75085c05f3fef57ebd77838f8c1
jssuzanne <js.suzanne@gmail.com>
2020-04-09 10:05:42 +0200

    Import conftest from anyblok and anyblok_pyramid

--- anyblok_furetui/furetui/tests/__init__.py
+++ anyblok_furetui/furetui/tests/__init__.py
@@ -6,13 +6,3 @@
 # This Source Code Form is subject to the terms of the Mozilla Public License,
 # v. 2.0. If a copy of the MPL was not distributed with this file,You can
 # obtain one at http://mozilla.org/MPL/2.0/.
-from anyblok.config import Configuration
-from anyblok import (
-        load_init_function_from_entry_points,
-        configuration_post_load,
-)
-
-
-load_init_function_from_entry_points(unittest=True)
-Configuration.load_config_for_test()
-configuration_post_load(unittest=True)
--- anyblok_furetui/furetui/tests/conftest.py
+++ anyblok_furetui/furetui/tests/conftest.py
@@ -1,10 +1,8 @@
 # This file is a part of the AnyBlok project
 #
-#    Copyright (C) 2020 Jean-Sebastien SUZANNE <js.suzanne@gmail.com>
+#    Copyright (C) 2019 Jean-Sebastien SUZANNE <js.suzanne@gmail.com>
 #
 # This Source Code Form is subject to the terms of the Mozilla Public License,
 # v. 2.0. If a copy of the MPL was not distributed with this file,You can
 # obtain one at http://mozilla.org/MPL/2.0/.
-import pytest  # noqa
-from anyblok.conftest import *  # noqa
-from anyblok_pyramid.conftest import *  # noqa
+from anyblok.tests.conftest import *  # noqa
