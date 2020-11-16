677108b11769365965cee4e1cfcb4f98917f0325
jssuzanne <js.suzanne@gmail.com>
2020-11-16 16:12:03 +0100

    Added options

--- anyblok_furetui/furetui/furetui.py
+++ anyblok_furetui/furetui/furetui.py
@@ -137,19 +137,47 @@ class FuretUI:
         return True
 
     @classmethod
+    def get_exposed_method_options(cls, request, resource, model, call, data,
+                                   pks):
+
+        def get_resource():
+            if resource == '0':
+                return None
+
+            return cls.registry.FuretUI.Resource.query().get(int(resource))
+
+        res = [
+            ('request', request),
+            ('authenticated_userid', request.authenticated_userid),
+            ('resource', get_resource),
+        ]
+        return res
+
+    @classmethod
     def call_exposed_method(cls, request, resource=None, model=None, call=None,
                             data=None, pks=None):
         if call not in cls.registry.exposed_methods.get(model, {}):
             raise HTTPUnauthorized(f"the method '{call}' is not exposed")
 
+        def apply_value(value):
+            return value() if callable(value) else value
+
+        options = {}
         definition = cls.registry.exposed_methods[model][call]
-        args = []
         obj = cls.registry.get(model)
         if definition['is_classmethod'] is False:
             obj = obj.from_primary_keys(**pks)
 
+        for (key, value) in cls.get_exposed_method_options(
+            request, resource, model, call, data, pks
+        ):
+            if definition[key] is True:
+                options[key] = apply_value(value)
+            elif definition[key]:
+                options[definition[key]] = apply_value(value)
+
         res = None
         with saved_errors_in_request(request):
-            res = getattr(obj, call)(*args, **data)
+            res = getattr(obj, call)(**options, **data)
 
         return res
--- anyblok_furetui/tests/test_call_method.py
+++ anyblok_furetui/tests/test_call_method.py
@@ -29,16 +29,29 @@ def add_core_in_registry():
         def on_method(self, param=None):
             pass
 
-        @exposed_method(request='request')
-        def with_request(cls, request, param=None):
+        @exposed_method(request=True)
+        def with_request(cls, request=None, param=None):
+            pass
+
+        @exposed_method(request='request2')
+        def with_request2(cls, request2=None, param=None):
+            pass
+
+        @exposed_method(authenticated_userid=True)
+        def with_authenticated_userid(cls, authenticated_userid=None,
+                                      param=None):
             pass
 
         @exposed_method(authenticated_userid='user')
-        def with_authenticated_userid(cls, user, param=None):
+        def with_authenticated_userid2(cls, user=None, param=None):
             pass
 
-        @exposed_method(resource='resource')
-        def with_resource(cls, resource, param=None):
+        @exposed_method(resource=True)
+        def with_resource(cls, resource=None, param=None):
+            pass
+
+        @exposed_method(resource='resource2')
+        def with_resource2(cls, resource2=None, param=None):
             pass
 
 
@@ -55,58 +68,33 @@ def add_mixin_in_registry():
         def on_method(self, param=None):
             pass
 
-        @exposed_method(request='request')
-        def with_request(cls, request, param=None):
+        @exposed_method(request=True)
+        def with_request(cls, request=None, param=None):
+            pass
+
+        @exposed_method(request='request2')
+        def with_request2(cls, request2=None, param=None):
+            pass
+
+        @exposed_method(authenticated_userid=True)
+        def with_authenticated_userid(cls, authenticated_userid=None,
+                                      param=None):
             pass
 
         @exposed_method(authenticated_userid='user')
-        def with_authenticated_userid(cls, user, param=None):
+        def with_authenticated_userid2(cls, user=None, param=None):
             pass
 
-        @exposed_method(resource='resource')
-        def with_resource(cls, resource, param=None):
+        @exposed_method(resource=True)
+        def with_resource(cls, resource=None, param=None):
+            pass
+
+        @exposed_method(resource='resource2')
+        def with_resource2(cls, resource2=None, param=None):
             pass
 
 
-def add_model_in_registry():
-
-    @register(Model)
-    class Test:
-
-        @exposed_method(is_classmethod=True)
-        def on_classmethod(cls, param=None):
-            return super(Test, cls).on_classmethod(param=param)
-
-        @exposed_method(is_classmethod=False)
-        def on_method(self, param=None):
-            return super(Test, self).on_method(param=param)
-
-        @exposed_method(request='request')
-        def with_request(cls, request, param=None):
-            return super(Test, cls).with_request(request, param=param)
-
-        @exposed_method(authenticated_userid='user')
-        def with_authenticated_userid(cls, user, param=None):
-            return super(Test, cls).with_authenticated_userid(
-                user, param=param)
-
-        @exposed_method(resource='resource')
-        def with_resource(cls, resource, param=None):
-            return super(Test, cls).with_resource(resource, param=param)
-
-
-def _with_call_method(oncore=False, onmixin=False, onmodel=False):
-
-    if oncore:
-        add_core_in_registry()
-
-    @register(Mixin)
-    class MixinTest:
-        pass
-
-    if onmixin:
-        add_mixin_in_registry()
-
+def add_model_in_registry(MixinTest):
     @register(Model)
     class Test(MixinTest):
         code = String(primary_key=True)
@@ -122,19 +110,91 @@ def _with_call_method(oncore=False, onmixin=False, onmodel=False):
             return self.code, param
 
         @classmethod
-        def with_request(cls, request, param=None):
+        def with_request(cls, request=None, param=None):
             return request.anyblok.registry.db_name, param
 
         @classmethod
-        def with_authenticated_userid(cls, user, param=None):
+        def with_request2(cls, request2=None, param=None):
+            return request2.anyblok.registry.db_name, param
+
+        @classmethod
+        def with_authenticated_userid(cls, authenticated_userid=None,
+                                      param=None):
+            return authenticated_userid, param
+
+        @classmethod
+        def with_authenticated_userid2(cls, user, param=None):
             return user, param
 
         @classmethod
         def with_resource(cls, resource, param=None):
-            return resource.id, param
+            return resource.id if resource else resource, param
+
+        @classmethod
+        def with_resource2(cls, resource2, param=None):
+            return resource2.id, param
+
+
+def add_model_with_decorator_in_registry():
+    @register(Model)
+    class Test:
+
+        @exposed_method(is_classmethod=True)
+        def on_classmethod(cls, param=None):
+            return super(Test, cls).on_classmethod(param=param)
+
+        @exposed_method(is_classmethod=False)
+        def on_method(self, param=None):
+            return super(Test, self).on_method(param=param)
+
+        @exposed_method(request=True)
+        def with_request(cls, request=None, param=None):
+            return super(Test, cls).with_request(
+                request=request, param=param)
+
+        @exposed_method(request='request2')
+        def with_request2(cls, request2=None, param=None):
+            return super(Test, cls).with_request2(
+                request2=request2, param=param)
+
+        @exposed_method(authenticated_userid=True)
+        def with_authenticated_userid(cls, authenticated_userid=None,
+                                      param=None):
+            return super(Test, cls).with_authenticated_userid(
+                authenticated_userid=authenticated_userid, param=param)
+
+        @exposed_method(authenticated_userid='user')
+        def with_authenticated_userid2(cls, user=None, param=None):
+            return super(Test, cls).with_authenticated_userid2(
+                user=user, param=param)
+
+        @exposed_method(resource=True)
+        def with_resource(cls, resource=None, param=None):
+            return super(Test, cls).with_resource(
+                resource=resource, param=param)
+
+        @exposed_method(resource='resource2')
+        def with_resource2(cls, resource2=None, param=None):
+            return super(Test, cls).with_resource2(
+                resource2=resource2, param=param)
+
+
+def _with_call_method(oncore=False, onmixin=False, onmodel=False):
+
+    if oncore:
+        add_core_in_registry()
+
+    @register(Mixin)
+    class MixinTest:
+        pass
+
+    if onmixin:
+        add_mixin_in_registry()
+
+    add_model_in_registry(MixinTest)
 
     if onmodel:
-        add_model_in_registry()
+        add_model_with_decorator_in_registry()
 
 
 KWARGS = [
@@ -193,21 +253,41 @@ class TestCallMethod:
 
     def test_decorated_with_request(self, webserver, registry_call_method):
         response = self.call(webserver, 'with_request')
-        assert response.json_body == (get_db_name(), 1)
+        assert response.json_body == [get_db_name(), 1]
+
+    def test_decorated_with_request2(self, webserver, registry_call_method):
+        response = self.call(webserver, 'with_request2')
+        assert response.json_body == [get_db_name(), 1]
 
     def test_decorated_with_authenticated_user(self, webserver,
                                                registry_call_method):
         response = self.call(webserver, 'with_authenticated_userid')
-        assert response.json_body == ('test', 1)
+        assert response.json_body == ['test', 1]
 
-    def test_decorated_with_resource(self, webserver, registry_call_method):
-        resource = registry_call_method.FuretUI.Resource.Custom.insert(
-            component='test')
-        call = 'with_resource'
+    def test_decorated_with_authenticated_user2(self, webserver,
+                                                registry_call_method):
+        response = self.call(webserver, 'with_authenticated_userid2')
+        assert response.json_body == ['test', 1]
+
+    def call_with_resource(self, registry, webserver, call):
+        resource = registry.FuretUI.Resource.Custom.insert(component='test')
         url = f'/furet-ui/resource/{resource.id}/model/Model.Test/call/{call}'
         params = {'data': {'param': 1}, 'pks': {'code': 'test'}}
         response = webserver.post_json(url, params)
-        assert response.json_body == (resource.id, 1)
+        assert response.json_body == [resource.id, 1]
+
+    def test_decorated_with_resource(self, webserver, registry_call_method):
+        self.call_with_resource(
+            registry_call_method, webserver, 'with_resource')
+
+    def test_decorated_with_resource_is_0(self, webserver,
+                                          registry_call_method):
+        response = self.call(webserver, 'with_resource')
+        assert response.json_body == [None, 1]
+
+    def test_decorated_with_resource2(self, webserver, registry_call_method):
+        self.call_with_resource(
+            registry_call_method, webserver, 'with_resource2')
 
     @pytest.mark.skip()
     def test_decorated_with_permission(self, webserver, registry_call_method):
