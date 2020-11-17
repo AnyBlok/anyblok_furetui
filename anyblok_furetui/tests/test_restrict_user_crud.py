cea87e3e811dc87b1d7fc0a6083947e06505b53c
Pierre Verkest <pierreverkest84@gmail.com>
2020-11-17 16:42:40 +0100

    styling

--- anyblok_furetui/tests/test_restrict_user_crud.py
+++ anyblok_furetui/tests/test_restrict_user_crud.py
@@ -1,17 +1,18 @@
+from urllib.parse import urlencode
+
 import pytest
 from anyblok import Declarations
 from anyblok.column import Integer, String
 from anyblok.relationship import Many2One
-from anyblok.tests.conftest import (
-    init_registry_with_bloks,
-    bloks_loaded,
+from anyblok.tests.conftest import (  # noqa F401
     base_loaded,
+    bloks_loaded,
+    init_registry_with_bloks,
 )
-# from pyramid.testing import DummyRequest
-from pyramid.request import Request
-from urllib.parse import urlencode
 from anyblok_pyramid.bloks.pyramid.restrict import restrict_query_by_user
 
+# from pyramid.testing import DummyRequest
+from pyramid.request import Request
 
 register = Declarations.register
 Model = Declarations.Model
@@ -19,14 +20,15 @@ Mixin = Declarations.Mixin
 
 
 class DummyRequest(Request):
-     authenticated_userid = None
-     def __init__(self, login, *args, **kwargs):
-         self.authenticated_userid = login
-         super().__init__(*args, **kwargs)
+    authenticated_userid = None
+
+    def __init__(self, login, *args, **kwargs):
+        self.authenticated_userid = login
+        super().__init__(*args, **kwargs)
 
 
 @pytest.fixture(scope="module")
-def setup_registry(request, bloks_loaded):
+def setup_registry(request, bloks_loaded):  # noqa F811
     def setup(bloks, function, **kwargs):
         registry = init_registry_with_bloks(bloks, function, **kwargs)
         request.addfinalizer(registry.close)
@@ -36,18 +38,14 @@ def setup_registry(request, bloks_loaded):
 
 
 def add_model_in_registry(*args, **kwargs):
-
-
     @register(Model)
     class Team:
         id = Integer(primary_key=True)
         name = String()
 
-
     @register(Model.Pyramid)
     class User:
         team = Many2One(model=Model.Team)
-
 
     @register(Mixin)
     class TeamOwner:
@@ -57,12 +55,7 @@ def add_model_in_registry(*args, **kwargs):
         def restric_by_user_team(cls, query, user):
             Team = cls.registry.Team
             User = cls.registry.Pyramid.User
-            return (
-                query.join(Team)
-                    .join(User)
-                    .filter(User.login == user.login)
-            )
-
+            return query.join(Team).join(User).filter(User.login == user.login)
 
     @register(Model)
     class Order(TeamOwner):
@@ -71,15 +64,13 @@ def add_model_in_registry(*args, **kwargs):
 
 
 def shared_data(registry):
-    team1 = registry.Team.insert(name='Team 1')
-    team2 = registry.Team.insert(name='Team 2')
-    registry.Pyramid.User.insert(login='user1', team=team1)
-    registry.Pyramid.User.insert(login='user2', team=team2)
-    registry.Order.insert(name='Order 1.1', team=team1)
-    registry.Order.insert(name='Order 2.1', team=team2)
-    registry.Order.insert(name='Order 2.2', team=team2)
-
-
+    team1 = registry.Team.insert(name="Team 1")
+    team2 = registry.Team.insert(name="Team 2")
+    registry.Pyramid.User.insert(login="user1", team=team1)
+    registry.Pyramid.User.insert(login="user2", team=team2)
+    registry.Order.insert(name="Order 1.1", team=team1)
+    registry.Order.insert(name="Order 2.1", team=team2)
+    registry.Order.insert(name="Order 2.2", team=team2)
 
 
 @pytest.fixture(scope="module")
@@ -107,10 +98,10 @@ def test_restrict_read_by_user(registry):
                 "QUERY_STRING": urlencode(
                     query={
                         "context[model]": "Model.Order",
-                        "context[fields]": "name"
+                        "context[fields]": "name",
                     }
                 )
-            }
+            },
         )
     )
     assert resutls["total"] == 1
@@ -125,11 +116,14 @@ def test_restrict_read_by_user2(registry):
                 "QUERY_STRING": urlencode(
                     query={
                         "context[model]": "Model.Order",
-                        "context[fields]": "name"
+                        "context[fields]": "name",
                     }
                 )
-            }
+            },
         )
     )
     assert resutls["total"] == 2
-    assert [d["data"]["name"] for d in resutls["data"]] == ["Order 2.1", "Order 2.2"]
+    assert [d["data"]["name"] for d in resutls["data"]] == [
+        "Order 2.1",
+        "Order 2.2",
+    ]
