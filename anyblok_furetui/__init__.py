119a65ee6fec7b22de4c9026de76cd5f64a147d3
Pierre Verkest <pierreverkest84@gmail.com>
2020-11-24 16:09:41 +0100

    add merge method to deep update 2 dicts

--- anyblok_furetui/__init__.py
+++ anyblok_furetui/__init__.py
@@ -7,5 +7,26 @@
 # This Source Code Form is subject to the terms of the Mozilla Public License,
 # v. 2.0. If a copy of the MPL was not distributed with this file,You can
 # obtain one at http://mozilla.org/MPL/2.0/.
-
+from collections.abc import Mapping
+from copy import deepcopy
 from .security import exposed_method  # noqa
+
+
+def merge(dict1, dict2):
+    """Return a new dictionary by merging two dictionaries recursively
+    In an immutable way
+
+    list are merged as well
+    """
+    result = deepcopy(dict1)
+    for key, value in dict2.items():
+        if isinstance(value, Mapping) and isinstance(
+            result.get(key, {}), Mapping
+        ):
+            result[key] = merge(result.get(key, {}), value)
+        elif isinstance(value, list) and isinstance(result.get(key, []), list):
+            result[key] = result.get(key, [])
+            result[key].extend(dict2[key])
+        else:
+            result[key] = deepcopy(dict2[key])
+    return result
