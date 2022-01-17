12ea391ee042784e437e4e6181cca110bc001622
jssuzanne <js.suzanne@gmail.com>
2022-01-17 23:37:42 +0100

    Fixed some unittest

--- anyblok_furetui/furetui/tests/test_view_space.py
+++ anyblok_furetui/furetui/tests/test_view_space.py
@@ -29,40 +29,12 @@ class TestViewSpace:
                                     parent_id=root.id)
         return (space.code, menu.id, resource.id)
 
-    def test_get_spaces(self, webserver, rollback_registry):
-        (space, menu, resource) = self.import_space_definition(
-            rollback_registry)
-        response = webserver.get('/furet-ui/spaces')
-        assert response.json_body == [
-            {
-                'menus': [
-                    {
-                        'code': 'test',
-                        'description': 'Test',
-                        'icon': {
-                            'code': None,
-                            'type': None
-                        },
-                        'label': 'Test',
-                        'path': (
-                            f'/space/{space}/menu/{menu}/resource/{resource}?'
-                        )
-                    }
-                ],
-                'type': 'UPDATE_SPACE_MENUS'
-            },
-        ]
-
     def test_get_space(self, webserver, rollback_registry):
         (space, menu, resource) = self.import_space_definition(
             rollback_registry)
         m = rollback_registry.FuretUI.Menu.query().get(menu)
         response = webserver.get(f'/furet-ui/space/{space}')
         assert response.json_body == [
-            {
-                'label': 'Test',
-                'type': 'UPDATE_CURRENT_SPACE'
-            },
             {
                 'menus': [
                     {
